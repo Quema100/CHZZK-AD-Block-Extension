0f3a9f2568c22c029af6596d797ace54ed10473b
Quema <myoon0929@gmail.com>
2024-06-26 01:13:49 +0900

    mistake

--- skip.js
+++ skip.js
@@ -1,10 +1,12 @@
 let adskip = () => {
+    console.log("Working")
     const adskip_button = document.querySelector("button[aria-label='광고 SKIP']")
     if (adskip_button != null || adskip_button != undefined){
         adskip_button.click()
         console.log("skip ad")
     }
 }
+window.onload = adskip
 setInterval(()=>{
-    adskip
+    adskip()
 },100)
