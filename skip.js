e11c90c192c6182920955dcb5f7a1db9717c0cf0
Quema <myoon0929@gmail.com>
2024-06-26 01:04:53 +0900

    fix

--- skip.js
+++ skip.js
@@ -2,6 +2,9 @@ let adskip = () => {
     const adskip_button = document.querySelector("button[aria-label='광고 SKIP']")
     if (adskip_button != null || adskip_button != undefined){
         adskip_button.click()
+        console.log("skip ad")
     }
 }
-window.onload=adskip
+setInterval(()=>{
+    adskip
+},100)
