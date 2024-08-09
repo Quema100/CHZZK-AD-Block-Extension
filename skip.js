02e9f9bd5a1b3c8c30b9136fd3b035e253fa0a3c
Quema <myoon0929@gmail.com>
2024-08-10 03:15:33 +0900

    fix

--- skip.js
+++ skip.js
@@ -5,12 +5,6 @@ let adskip = () => {
         adskip_button.click()
         console.log("skip ad")
     }
-
-    const ad_Div = document.querySelector("div[class='live_end_banner_container__I+pPM']")??document.querySelector("div[class='home_banner_container__FzOBB']")
-    if(ad_Div){
-        ad_Div.remove()
-        console.log("ad rm");
-    }
 }
 window.onload = adskip
 setInterval(()=>{
